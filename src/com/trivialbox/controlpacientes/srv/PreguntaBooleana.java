fbffe26eaf3d3d1fc53f1ff401d6ea7c08d869ae
root <root@localhost.localdomain>
2016-07-22 12:38:09 -0500

    implementacion de clase PreguntaBoolean

--- src/com/trivialbox/controlpacientes/srv/PreguntaBooleana.java
+++ src/com/trivialbox/controlpacientes/srv/PreguntaBooleana.java
@@ -1,9 +1,42 @@
 package com.trivialbox.controlpacientes.srv;
 
+import com.trivialbox.controlpacientes.srv.exceptions.PreguntaBooleanaNoRespondida;
+
 /**
  *
  * @author root
  */
-public class PreguntaBooleana {
+public class PreguntaBooleana extends Pregunta<Boolean, Boolean>{
+    private Boolean respuesta = null;
     
+    public PreguntaBooleana(int id, String titulo, boolean esOpcional){
+        super(id, titulo, esOpcional);
+    }
+
+    @Override
+    public void responder(Boolean respuesta) {
+        if(esNula(respuesta))
+            throw new PreguntaBooleanaNoRespondida(getId());
+        this.respuesta = respuesta;
+    }
+
+    @Override
+    protected Boolean respuesta() {
+        return this.respuesta;
+    }
+
+    @Override
+    protected Boolean getRespuestaPorDefecto() {
+        return null;
+    }
+
+    @Override
+    protected boolean preguntaRespondida() {
+        return this.respuesta != null;
+    }
+
+    private boolean  esNula(Boolean respuesta){
+        return respuesta == null;
+    }
+        
 }
