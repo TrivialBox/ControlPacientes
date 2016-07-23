31af5492f332129541cb142584434515d5a8a0ab
Krysthyan <krysthyan_09h@yahoo.com>
2016-07-23 00:57:43 -0500

    Ventana de login

--- src/com/trivialbox/controlpacientes/ControlPacientes.java
+++ src/com/trivialbox/controlpacientes/ControlPacientes.java
@@ -1,9 +1,25 @@
 package com.trivialbox.controlpacientes;
 
+import com.trivialbox.controlpacientes.gui.Login;
+import javax.swing.UIManager;
+
 public class ControlPacientes {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            //Look and feel para sistemas linux con gtk
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel"); 
+        } 
+        catch(Exception e){
+            try {
+                //Look and feel para sistemas windows
+                UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+            } catch (Exception ex) {
+                //Carga interfaz por defecto de java swing
+            }
+        }
+        Login login = new Login();
+        login.setVisible(true);
     }
     
 }
---
+++ src/com/trivialbox/controlpacientes/gui/MensajeHolder.java
@@ -0,0 +1,38 @@
+package com.trivialbox.controlpacientes.gui;
+
+
+public class MensajeHolder {
+    private String cedula = "Número de cédula";
+    private String password = "1234";
+
+    /**
+     * @return the cedula
+     */
+    public String getCedula() {
+        return cedula;
+    }
+
+    /**
+     * @param cedula the cedula to set
+     */
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
+}
