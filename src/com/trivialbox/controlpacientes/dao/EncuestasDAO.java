b1d9f1d3f5f03f16f54cf3bae70f917311cdd9cd
Santos Gallegos <santos_g@outlook.com>
2016-07-24 23:00:45 +0200

    Implementados métodos de dao encuesta

--- src/com/trivialbox/controlpacientes/dao/EncuestasDAO.java
+++ src/com/trivialbox/controlpacientes/dao/EncuestasDAO.java
@@ -1,7 +1,13 @@
 package com.trivialbox.controlpacientes.dao;
 
 import com.trivialbox.controlpacientes.dao.db.DataBase;
+import com.trivialbox.controlpacientes.dao.db.ObjectField;
+import com.trivialbox.controlpacientes.dao.db.RowDB;
+import com.trivialbox.controlpacientes.dao.db.TablaDB;
 import com.trivialbox.controlpacientes.srv.objetos.Encuesta;
+import com.trivialbox.controlpacientes.srv.objetos.Tools;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class EncuestasDAO {
@@ -20,22 +26,72 @@ public class EncuestasDAO {
     }
     
     public Encuesta get(String nombre) {
-        return null;
+        Encuesta encuesta;
+        String[] tables = new String[]{"Encuesta"};
+        TablaDB result = dataBase.select(Arrays.asList(tables), "nombre", nombre);
+        encuesta = construirEncuesta(result, 0);
+        return encuesta;
     }
     
     public void del(String nombre) {
-        
+        throw new RuntimeException("Borrar una encuesta aún no está disponible");
     }
     
-    public void add(Encuesta encuesta) {
-        
+    public Encuesta add(Encuesta encuesta) {
+        try {
+            List<ObjectField> fieldsEncuesta = Tools.getValues(encuesta, getExcepciones());
+            dataBase.insert(
+                    Encuesta.class.getSimpleName(),
+                    fieldsEncuesta
+            );
+            
+            return get(encuesta.getNombre());
+        } catch (Exception ex) {
+            throw new RuntimeException("Error al agregar encuesta." + ex.getMessage());
+        }
     }
     
-    public void update(String nombre, Encuesta encuesta) {
-        
+    public Encuesta update(String nombre, Encuesta encuesta) {
+        try {
+            Encuesta encuestaOld = get(nombre);
+            List<ObjectField> fieldsEncuesta = Tools.getValues(encuesta, getExcepciones());
+            List<ObjectField> fieldsOldEncuesta = Tools.getValues(encuestaOld, null);
+
+            dataBase.update(
+                    Encuesta.class.getSimpleName(),
+                    fieldsOldEncuesta,
+                    fieldsEncuesta
+            );
+            
+            return get(nombre);
+        } catch(Exception e) {
+            throw new RuntimeException("Error al actualizar encuesta." + e.getMessage());
+        }
     }
     
     public List<Encuesta> get() {
-        return null;
+        List<Encuesta> encuestas = new ArrayList<>();
+        TablaDB result = dataBase.getAll("Encuesta");
+        int index = 0;
+        for (RowDB r : result)
+            encuestas.add(construirEncuesta(result, index++));
+        return encuestas;
+    }
+
+    private Encuesta construirEncuesta(TablaDB result, int index) {
+        Encuesta encuesta;
+        RowDB r = result.getRow(index);
+
+        encuesta = new Encuesta(
+                Integer.parseInt(r.getField(0)),
+                r.getField(1)
+        );
+        encuesta.setDescripcion(r.getField(2));
+        return encuesta;
+    }
+
+    private ArrayList<String> getExcepciones() {
+        String[] exceptions = new String[] {"idEncuesta"};
+        return new ArrayList<>(Arrays.asList(exceptions));
     }
 }
