4241999e357bfc41f30dd836a68488afc436e44a
Santos Gallegos <santos_g@outlook.com>
2016-07-25 05:42:35 +0200

    Import inútil

--- src/com/trivialbox/controlpacientes/dao/PreguntasDAO.java
+++ src/com/trivialbox/controlpacientes/dao/PreguntasDAO.java
@@ -16,7 +16,6 @@ import com.trivialbox.controlpacientes.srv.objetos.Rango;
 import com.trivialbox.controlpacientes.srv.objetos.Tools;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 
 public class PreguntasDAO {
