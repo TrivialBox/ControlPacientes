046ef337835a7f9a87e1c03f5690a67d8100fd82
Santos Gallegos <santos_g@outlook.com>
2016-07-25 04:41:05 +0200

    implementada pregunta fecha

--- src/com/trivialbox/controlpacientes/srv/objetos/PreguntaFecha.java
+++ src/com/trivialbox/controlpacientes/srv/objetos/PreguntaFecha.java
@@ -1,9 +1,32 @@
 package com.trivialbox.controlpacientes.srv.objetos;
 
-/**
- *
- * @author root
- */
-public class PreguntaFecha {
+import java.util.Calendar;
+
+public class PreguntaFecha extends Pregunta<Calendar, String> {
+    private String respuesta = null;
+
+    public PreguntaFecha(int id, int idEncuesta, String titulo, boolean esOpcional) {
+        super(id, idEncuesta, titulo, esOpcional);
+    }
+
+    @Override
+    public void responder(Calendar respuesta) {
+        this.respuesta = Tools.calendarToDateString(respuesta);
+    }
+
+    @Override
+    protected String respuesta() {
+        return this.respuesta;
+    }
+
+    @Override
+    protected String getRespuestaPorDefecto() {
+        return "";
+    }
+
+    @Override
+    protected boolean preguntaRespondida() {
+        return this.respuesta != null;
+    }
     
 }
